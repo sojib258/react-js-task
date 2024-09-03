a36c32dd948cc9758385fae29c31f9018b02ef50
Sajib Hasan <sojibsrd85@gmail.com>
2024-09-03 12:47:37 +0600

    setup galliard font

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ function App() {
 
   return (
     <>
-      <h2>Let's Start the Project with Fun and Professional Way</h2>
+      <button className="button">See all reviews</button>
+      <h2 className='heading'>Liposomal Vitamin C</h2>
+      <p className="paraOne">Liposomal Vitamin C is an advanced form of Vitamin C encapsulated within liposomes, which are tiny fat-like particles that enhance its absorption and bioavailability in the body. </p>
+      <p className="paraTwo">This delivery method allows for more efficient cellular uptake, ensuring that higher levels of Vitamin C reach the bloodstream and tissues.</p>
+      <p className="paraThree">It provides potent antioxidant support, boosts the immune system, promotes collagen production, and enhances skin health. Liposomal Vitamin C is ideal for those seeking maximum benefits from their Vitamin C supplementation.</p>
     </>
   )
 }
