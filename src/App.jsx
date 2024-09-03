693c1122eb0798746a9d6c098411738ebb3c9532
Sajib Hasan <sojibsrd85@gmail.com>
2024-09-03 07:55:47 +0600

    project start from here

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Let's Start the Project with Fun and Professional Way</h2>
+    </>
+  )
+}
+
+export default App
